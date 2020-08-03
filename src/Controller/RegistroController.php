b72f3f579b09d01444512ecd5d5055a583386ab2
Javier Gonzalez <jgalvarez.jga@gmail.com>
2020-08-03 15:10:15 +0200

    capitulo_8

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -5,17 +5,48 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+//Importamos el formulario UserType y la entidad
+use App\Form\UserType;
+use App\Entity\User;
+//importar la libreria request
+use Symfony\Component\HttpFoundation\Request;
+
 class RegistroController extends AbstractController
 {
     /**
      * @Route("/registro", name="registro")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $user = new User;   //objeto de la clase user                     
+        $user->setRoles(['ROLE_USER']);   //tipo array
+        $user->setBaneado(False);  //asigno el valor false en la grabacion. Campo obligatorio  
+        $form = $this->createForm(UserType::class, $user);                
+
+        $form->handleRequest($request);        
+        if ($form->isSubmitted() && $form->isValid()) {                
+            //guardar los datos en la base de datos
+            //---------------------------------------------
+            //recoger los datos del forumlario
+            $form->getData();
+            //pasar los datos del formulario a una variable
+            $grabar = $form->getData();        
+            //Gruadar con Doctrine
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($grabar);
+            $entityManager->flush();            
+            $this->addFlash('mensaje', 'Tus cambios se han guardado!');
+
+            //ir a otra pagina
+            //return $this->redirectToRoute('task_success');
+        }
+        
+        //renderizar un formualrio, despues del createview()
         return $this->render('registro/index.html.twig', [
             'controller_name' => 'Hola mundo',
             'nombre' => 'javier',
             'apellidos' => 'gonzalez alvarez',
-        ]);
+            'formulario' => $form->createview()
+        ]);             
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,16 +7,24 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+//Importo los tipos de datos para el formulario
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\JsonType;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('baneado')
-            ->add('nombre')
+            ->add('email', TextType::class)
+            //->add('roles', JsonType::class)  //No lo necesito en el alta
+            ->add('password', PasswordType::class)
+            //->add('baneado', BooleanType::class) //No lo necesito en el alta
+            ->add('nombre', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Grabar registro'))            
         ;
     }
 
